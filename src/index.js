501838455ffecf1fbedd8b75b187d616366be507
wajeehgul@gmail.com <wajeehgul@gmail.com>
2024-03-22 08:20:56 +0500

    Day 1 Challenge 4

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 
-const currentDate = new Date().toLocaleDateString();
-const currentTime = new Date().toLocaleTimeString();
+const myName = "Wajiha Gul";
+const img1 = "https://picsum.photos/id/1/200/300";
+const img2 = "https://picsum.photos/id/1/250/300";
+const img3 = "https://picsum.photos/id/1/300/300";
+
 ReactDOM.render(
   <>
-    <h1>Wajiha Gul</h1>
-    <h2> Today is {currentDate}</h2>
-    <h2>Now the Time is {currentTime} </h2>
+    <h1 className="textColor"> My Name is {myName}</h1>
+    <p className="textPColor">Uploading Random Images</p>
+    <div className="divImg">
+      <img src={img1} alt="Random Image 1" />
+      <img src={img2} alt="Random Image 2" />
+      <img src={img3} alt="Random Image 3" />
+    </div>
   </>,
   document.getElementById("root")
 );
