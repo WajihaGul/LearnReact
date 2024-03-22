8df13458e1f187072626f7418c85a54828cec904
wajeehgul@gmail.com <wajeehgul@gmail.com>
2024-03-22 07:55:56 +0500

    Day 1 Challenge 3

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const myName = "Wajiha Gul";
-const img1 = "https://picsum.photos/id/1/200/300";
-const img2 = "https://picsum.photos/id/1/250/300";
-const img3 = "https://picsum.photos/id/1/300/300";
-const link = "https://chat.openai.com/c/ae134569-d390-4485-aa00-0a712b65b144";
-
+const currentDate = new Date().toLocaleDateString();
+const currentTime = new Date().toLocaleTimeString();
 ReactDOM.render(
   <>
-    <h1 contentEditable="true"> My Name is {myName}</h1>
-    <p>Uploading Random Images</p>
-    <img src={img1} alt="Random Image 1" />
-    <img src={img2} alt="Random Image 2" />
-
-    <a href={link} target="_blank">
-      <img src={img3} alt="Random Image 3" />
-    </a>
+    <h1>Wajiha Gul</h1>
+    <h2> Today is {currentDate}</h2>
+    <h2>Now the Time is {currentTime} </h2>
   </>,
   document.getElementById("root")
 );
