21e9c0cc6fe12caf003f0a1320db526481e6c5e3
wajeehgul@gmail.com <wajeehgul@gmail.com>
2024-03-23 07:07:45 +0500

    Greet Website(Day 2 Challenge 1)

--- src/index.js
+++ src/index.js
@@ -2,19 +2,32 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-const myName = "Wajiha Gul";
-const img1 = "https://picsum.photos/id/1/200/300";
-const img2 = "https://picsum.photos/id/1/250/300";
-const img3 = "https://picsum.photos/id/1/300/300";
-
+/*
+1. Create a React App 
+2. Show a heading that says Hello Sir, Good Morning,        
+    if time is between 1 to 11 am
+3. Good Afternoon if 12 PM to 7 PM
+4. Good Night, if 7 PM till midnight
+5. Apply inline CSS Style
+*/
+const cssStyle = {};
+const timeNow = new Date().getHours();
+if (timeNow > 1 && timeNow <= 11) {
+  var greet = "Good Morning";
+  cssStyle.color = "red";
+} else if (timeNow >= 12 && timeNow <= 18) {
+  var greet = "Good Afternoon";
+  cssStyle.color = "Green";
+} else {
+  var greet = "Good Night";
+  cssStyle.color = "Orange";
+}
 ReactDOM.render(
   <>
-    <h1 className="textColor"> My Name is {myName}</h1>
-    <p className="textPColor">Uploading Random Images</p>
-    <div className="divImg">
-      <img src={img1} alt="Random Image 1" />
-      <img src={img2} alt="Random Image 2" />
-      <img src={img3} alt="Random Image 3" />
+    <div>
+      <h1 className="greeth1">
+        HELLO SIR, <span style={cssStyle}> {greet}</span>
+      </h1>
     </div>
   </>,
   document.getElementById("root")
