77d040aa348a11e529c68770dd3f6dfac9852be1
wajeehgul@gmail.com <wajeehgul@gmail.com>
2024-03-23 12:32:20 +0500

    Calculator (Day 2 Challenge 2)

--- src/index.js
+++ src/index.js
@@ -1,33 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Sub, Sum, Division, Product } from "./Calc";
 import "./index.css";
 
-/*
-1. Create a React App 
-2. Show a heading that says Hello Sir, Good Morning,        
-    if time is between 1 to 11 am
-3. Good Afternoon if 12 PM to 7 PM
-4. Good Night, if 7 PM till midnight
-5. Apply inline CSS Style
-*/
-const cssStyle = {};
-const timeNow = new Date().getHours();
-if (timeNow > 1 && timeNow <= 11) {
-  var greet = "Good Morning";
-  cssStyle.color = "red";
-} else if (timeNow >= 12 && timeNow <= 18) {
-  var greet = "Good Afternoon";
-  cssStyle.color = "Green";
-} else {
-  var greet = "Good Night";
-  cssStyle.color = "Orange";
-}
 ReactDOM.render(
   <>
     <div>
-      <h1 className="greeth1">
-        HELLO SIR, <span style={cssStyle}> {greet}</span>
-      </h1>
+      <ul>
+        <li>Addition of a and b is {Sum(10, 20)}</li>
+        <li>Subtraction of a and b is {Sub(30, 20)}</li>
+        <li>Division of a and b is {Division(300, 20)}</li>
+        <li>Product of a and b is {Product(10, 20)}</li>
+      </ul>
     </div>
   </>,
   document.getElementById("root")
